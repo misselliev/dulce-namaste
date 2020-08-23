4ef145c39aa96a774e7c4dbe9abfdd45c8006be5
Elizabeth Villalejos <nekobanpaia@gmail.com>
2020-08-22 17:45:40 -0700

    Updating styling, adding images for background cards

---
+++ src/images/ImagesDir.tsx
@@ -0,0 +1,13 @@
+import sky1 from './sky1.jpg';
+import sky2 from './sky2.jpg';
+import sky3 from './sky3.jpg';
+import sky4 from './sky4.jpg';
+import sky5 from './sky5.jpg';
+
+export default {
+    sky1,
+    sky2,
+    sky3,
+    sky4,
+    sky5,
+};
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import './tailwind.output.css';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
