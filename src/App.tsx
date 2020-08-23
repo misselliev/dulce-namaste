98b74578c5a43dd41e6aacc41863c5de00c66971
Elizabeth Villalejos <nekobanpaia@gmail.com>
2020-08-22 17:46:42 -0700

    Updating Affirmation component to switch backgrounds, updating API link

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-import Affirmation from './components/Affirmation';
+import Dashboard from './components/Dashboard';
 
 const App: React.FC = () => {
     return (
         <div className="App">
-            <Affirmation />
+            <Dashboard />
         </div>
     );
 };
--- src/components/Affirmation.tsx
+++ src/components/Affirmation.tsx
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
+import Images from '../images/ImagesDir';
 const Affirmation: React.FC = () => {
     const [affirmation, setAffirmation] = useState('');
     async function getAffirmation() {
         await axios
-            .get('http://localhost:3000/affirmation')
+            .get('https://dulce-affirmations-api.herokuapp.com/affirmation')
             .then((res) => {
                 const response = JSON.stringify(Object.values(res.data[0])).replace(/[[\]"']+/g, '');
                 setAffirmation(response);
@@ -14,13 +14,30 @@ const Affirmation: React.FC = () => {
                 throw err;
             });
     }
+    function BackgroundSwitch() {
+        const backgrounds = [Images.sky1, Images.sky2, Images.sky3, Images.sky4, Images.sky5];
+        const random = Math.floor(Math.random() * backgrounds.length);
+        const selectedBackground = backgrounds[random];
+        const card = document.getElementById('card');
+        if (card) {
+            card.style.backgroundImage = `url(${selectedBackground})`;
+            card.style.backgroundSize = 'cover';
+        }
+    }
+
     useEffect(() => {
         getAffirmation();
+        BackgroundSwitch();
     }, []);
+
     return (
-        <div className="main">
-            {' '}
-            <div className="message">{affirmation}</div>
+        <div className="max-w-2xl rounded overflow-hidden shadow-2xl m-auto w-2/3" id="card">
+            <div className="p-20 text-center text-white">
+                <div className="font-bold text-xl mb-2">Daily Affirmation:</div>
+                <p id="message" className="font-bold text-base capitalize">
+                    {affirmation}
+                </p>
+            </div>
         </div>
     );
 };
